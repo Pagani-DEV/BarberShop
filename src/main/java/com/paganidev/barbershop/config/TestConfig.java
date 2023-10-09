dafa3077c0e6c203cd0f6932fe633c2d85c639a5
Pagani-DEV <contato.pagani>
2023-10-09 14:32:55 -0300

    Class Config Created

---
+++ src/main/java/com/paganidev/barbershop/config/TestConfig.java
@@ -0,0 +1,9 @@
+package com.paganidev.barbershop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+}
